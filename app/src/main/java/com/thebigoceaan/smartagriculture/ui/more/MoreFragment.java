6e8ddd7f23fcde54012d14adb81517eafd81751f
aprashant21 <aprashant21@tbc.edu.np>
2022-01-09 21:21:43 +0545

    farmer and normal user authorization added in fragment more

--- app/src/main/java/com/thebigoceaan/smartagriculture/ui/more/MoreFragment.java
+++ app/src/main/java/com/thebigoceaan/smartagriculture/ui/more/MoreFragment.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
 import com.thebigoceaan.smartagriculture.databinding.FragmentMoreBinding;
 import com.thebigoceaan.smartagriculture.hyperlink.DailyVegMarketActivity;
 import com.thebigoceaan.smartagriculture.hyperlink.WeatherInformationActivity;
+import com.thebigoceaan.smartagriculture.models.Farmer;
 import com.thebigoceaan.smartagriculture.services.news.NewsActivity;
 import com.thebigoceaan.smartagriculture.services.products.ProductDashboard;
 import com.thebigoceaan.smartagriculture.services.register.FarmerRegisterActivity;
@@ -19,6 +28,8 @@ import com.thebigoceaan.smartagriculture.services.register.FarmerRegisterActivit
 import java.util.Objects;
 
 public class MoreFragment extends Fragment {
+    FirebaseDatabase database;
+    FirebaseAuth auth;
 
     private FragmentMoreBinding binding;
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -67,7 +78,34 @@ public class MoreFragment extends Fragment {
             }
         });
 
+        //to get farmer database
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        binding.productDashboardBtn.setVisibility(View.GONE);
+        binding.registerAsFarmerDesc.setVisibility(View.GONE);
+        binding.imgRegisterBtn.setVisibility(View.GONE);
+        database.getReference("Farmer").child(auth.getCurrentUser().getUid()).get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                Farmer farmer = dataSnapshot.getValue(Farmer.class);
+                if(farmer!=null){
+                    binding.productDashboardBtn.setVisibility(View.VISIBLE);
+                    binding.registerAsFarmerDesc.setVisibility(View.GONE);
+                    binding.imgRegisterBtn.setVisibility(View.GONE);
+                }
+                else{
+                    binding.productDashboardBtn.setVisibility(View.GONE);
+                    binding.imgRegisterBtn.setVisibility(View.VISIBLE);
+                    binding.registerAsFarmerDesc.setVisibility(View.VISIBLE);
+                }
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 
