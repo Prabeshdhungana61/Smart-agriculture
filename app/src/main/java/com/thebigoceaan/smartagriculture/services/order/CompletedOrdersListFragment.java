9eaf2617ab383b8679d4700703e143d613b848de
aprashant21 <aprashant21@tbc.edu.np>
2022-01-09 19:34:45 +0545

    buyer order dashboard added

---
+++ app/src/main/java/com/thebigoceaan/smartagriculture/services/order/CompletedOrdersListFragment.java
@@ -0,0 +1,137 @@
+package com.thebigoceaan.smartagriculture.services.order;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
+import com.thebigoceaan.smartagriculture.R;
+import com.thebigoceaan.smartagriculture.adapters.OrderAdapter;
+import com.thebigoceaan.smartagriculture.databinding.FragmentCompletedOrdersListBinding;
+import com.thebigoceaan.smartagriculture.models.Order;
+
+import java.util.ArrayList;
+
+import es.dmoral.toasty.Toasty;
+
+public class CompletedOrdersListFragment extends Fragment {
+    FragmentCompletedOrdersListBinding binding;
+    private OrderAdapter adapter;
+    private ArrayList<Order> list = new ArrayList<Order>();
+    private boolean isLoading=false;
+    private CrudOrder crud = new CrudOrder();
+    String key = null;
+    private FirebaseAuth auth;
+    private OrderAdapter.RecyclerViewClickListener listener;
+    Dialog dialog;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding=FragmentCompletedOrdersListBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+
+        setOnClickListenerOrder();
+
+        auth = FirebaseAuth.getInstance();
+        binding.swipeCompletedOrder.setRefreshing(true);
+
+        binding.recyclerViewOrderListCompleted.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setReverseLayout(true);
+        manager.setStackFromEnd(true);
+        binding.recyclerViewOrderListCompleted.setLayoutManager(manager);
+        adapter = new OrderAdapter(getContext(),list,listener);
+        adapter.notifyDataSetChanged();
+        binding.recyclerViewOrderListCompleted.setAdapter(adapter);
+
+        dialog = new Dialog(getContext());
+
+        loadData();
+        binding.recyclerViewOrderListCompleted.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull @NotNull RecyclerView recyclerView, int dx, int dy) {
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager)binding.recyclerViewOrderListCompleted.getLayoutManager();
+                int totalItem=linearLayoutManager.getItemCount();
+                int lastVisible=linearLayoutManager.findLastCompletelyVisibleItemPosition();
+                if(totalItem<lastVisible+3){
+                    if(!isLoading){
+                        isLoading=true;
+                        loadData();
+                    }
+
+                }
+            }
+        });
+
+        return view;
+    }
+    private void loadData() {
+        crud.get(key).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                ArrayList<Order> order = new ArrayList<>();
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    Order order1 = data.getValue(Order.class);
+                    if (order1.getBuyerEmail().equals(auth.getCurrentUser().getEmail()) && order1.isCompleted()) {
+                        order1.setKey(data.getKey());
+                        order.add(order1);
+                        key = data.getKey();
+                    }
+
+                    binding.swipeCompletedOrder.setRefreshing(false);
+
+                }
+                adapter.setItem(order);
+                adapter.notifyDataSetChanged();
+                isLoading = false;
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                binding.swipeCompletedOrder.setRefreshing(false);
+                Toasty.error(getContext(), "" + error.getMessage(), Toast.LENGTH_SHORT,true).show();
+            }
+        });
+    }
+    public void setOnClickListenerOrder(){
+        listener=(view,position)->{
+            Order order = list.get(position);
+            dialog.setContentView(R.layout.dialog_confirmation);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            Button yes,no;
+            yes = dialog.findViewById(R.id.btn_yes_confirm);
+            no = dialog.findViewById(R.id.btn_no_confirm);
+            dialog.show();
+            yes.setOnClickListener(view1 -> {
+                crud.remove(order.getKey()).addOnSuccessListener(unused -> {
+                    dialog.dismiss();
+                    Intent intent = new Intent(getContext(), OrderList.class);
+                    startActivity(intent);
+                    Toasty.success(getContext(), "Successfully removed the order from the list!", Toasty.LENGTH_SHORT,true).show();
+                }).addOnFailureListener(e -> Toasty.error(getContext(), ""+e.getMessage(), Toasty.LENGTH_SHORT,true).show());
+            });
+            no.setOnClickListener(view12 -> {
+                dialog.dismiss();
+            });
+        };
+    }
+}
--- app/src/main/java/com/thebigoceaan/smartagriculture/services/order/OrderList.java
+++ app/src/main/java/com/thebigoceaan/smartagriculture/services/order/OrderList.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.Dialog;
 import android.content.Intent;
@@ -23,6 +24,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.annotations.NotNull;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import com.thebigoceaan.smartagriculture.R;
 import com.thebigoceaan.smartagriculture.adapters.OrderAdapter;
 import com.thebigoceaan.smartagriculture.databinding.ActivityOrderListBinding;
@@ -33,14 +37,6 @@ import es.dmoral.toasty.Toasty;
 
 public class OrderList extends AppCompatActivity {
     ActivityOrderListBinding binding;
-    private OrderAdapter adapter;
-    private ArrayList<Order> list = new ArrayList<Order>();
-    private boolean isLoading=false;
-    private CrudOrder crud = new CrudOrder();
-    String key = null;
-    private FirebaseAuth auth;
-    private OrderAdapter.RecyclerViewClickListener listener;
-    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,90 +50,17 @@ public class OrderList extends AppCompatActivity {
         ColorDrawable drawable =new ColorDrawable(ContextCompat.getColor(this, R.color.splashColor));
         actionBar.setBackgroundDrawable(drawable); //action bar ends
 
-        setOnClickListenerOrder();
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("PENDING ORDER", PendingOrderListFragment.class)
+                .add("COMPLETED ORDER", CompletedOrdersListFragment.class)
+                .create());
 
-        auth = FirebaseAuth.getInstance();
-        binding.swipOrder.setRefreshing(true);
+        ViewPager viewPager =  findViewById(R.id.viewpagerOrderList);
+        viewPager.setAdapter(adapter);
+        SmartTabLayout viewPagerTab =  findViewById(R.id.viewpagerTabOrderList);
+        viewPagerTab.setViewPager(viewPager);
 
-        binding.recyclerViewOrderList.setHasFixedSize(true);
-        LinearLayoutManager manager = new LinearLayoutManager(this);
-        manager.setReverseLayout(true);
-        manager.setStackFromEnd(true);
-        binding.recyclerViewOrderList.setLayoutManager(manager);
-        adapter = new OrderAdapter(this,list,listener);
-        adapter.notifyDataSetChanged();
-        binding.recyclerViewOrderList.setAdapter(adapter);
 
-        dialog = new Dialog(this);
-
-        loadData();
-        binding.recyclerViewOrderList.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull @NotNull RecyclerView recyclerView, int dx, int dy) {
-                LinearLayoutManager linearLayoutManager = (LinearLayoutManager)binding.recyclerViewOrderList.getLayoutManager();
-                int totalItem=linearLayoutManager.getItemCount();
-                int lastVisible=linearLayoutManager.findLastCompletelyVisibleItemPosition();
-                if(totalItem<lastVisible+3){
-                    if(!isLoading){
-                        isLoading=true;
-                        loadData();
-                    }
-
-                }
-            }
-        });
-    }
-
-    private void loadData() {
-        crud.get(key).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                ArrayList<Order> order = new ArrayList<>();
-                for (DataSnapshot data : snapshot.getChildren()) {
-                    Order order1 = data.getValue(Order.class);
-                    if (order1.getBuyerEmail().equals(auth.getCurrentUser().getEmail())) {
-                        order1.setKey(data.getKey());
-                        order.add(order1);
-                        key = data.getKey();
-                    }
-
-                    binding.swipOrder.setRefreshing(false);
-
-                }
-                adapter.setItem(order);
-                adapter.notifyDataSetChanged();
-                isLoading = false;
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-                binding.swipOrder.setRefreshing(false);
-                Toasty.error(OrderList.this, "" + error.getMessage(), Toast.LENGTH_SHORT,true).show();
-            }
-        });
-    }
-
-    public void setOnClickListenerOrder(){
-        listener=(view,position)->{
-          Order order = list.get(position);
-          dialog.setContentView(R.layout.dialog_confirmation);
-          dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-            Button yes,no;
-            yes = dialog.findViewById(R.id.btn_yes_confirm);
-            no = dialog.findViewById(R.id.btn_no_confirm);
-            dialog.show();
-            yes.setOnClickListener(view1 -> {
-                crud.remove(order.getKey()).addOnSuccessListener(unused -> {
-                    dialog.dismiss();
-                    Intent intent = new Intent(this, OrderList.class);
-                    startActivity(intent);
-                    Toasty.success(OrderList.this, "Successfully removed the order from the list!", Toasty.LENGTH_SHORT,true).show();
-                }).addOnFailureListener(e -> Toasty.error(OrderList.this, ""+e.getMessage(), Toasty.LENGTH_SHORT,true).show());
-            });
-            no.setOnClickListener(view12 -> {
-                dialog.dismiss();
-            });
-        };
     }
 }
